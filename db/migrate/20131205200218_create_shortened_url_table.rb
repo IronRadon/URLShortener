1b12b435eccf8a95e68b082b33d0c69044b21058
App Academy Student <student@appacademy.io>
2013-12-05 13:58:05 -0800

    Create user and shortenedurl tables and migration files

---
+++ db/migrate/20131205200218_create_shortened_url_table.rb
@@ -0,0 +1,16 @@
+class CreateShortenedUrlTable < ActiveRecord::Migration
+  def change
+    create_table :shortened_urls do |t|
+      t.text :long_url
+      t.string :short_url
+      t.integer :submitter_id
+
+      t.timestamps
+    end
+    add_index (:shortened_urls, :submitter_id)
+    add_index (:shortened_urls, :short_url, :unique => true)
+  end
+
+  def down
+  end
+end
