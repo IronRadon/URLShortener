e62710b5dc7b50fa011ab7b17aeb081507587602
IronRadon <wood.fern.l@gmail.com>
2013-12-05 17:50:47 -0800

    Finish TagTopic model

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -2,7 +2,7 @@ class TagTopic < ActiveRecord::Base
   attr_accessible :name
   validates :name, :presence => true, :uniqueness => true
 
-  has_many(:taggings
+  has_many(:taggings,
            :primary_key => :id,
            :foreign_key => :tag_topic_id,
            :class_name => 'Tagging')
